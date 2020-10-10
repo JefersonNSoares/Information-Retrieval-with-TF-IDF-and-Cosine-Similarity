0fa06349c777a01297de0065f7a47a7ca4f61046
gustavo <gustavorw03@gmail.com>
2020-10-10 17:05:46 -0300

    falta terminar de aplicar css

--- QA/app.py
+++ QA/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__ , static_url_path='/static')
 @app.route("/", methods=['GET'])
 def index():
     context = ''
-    question = request.args.get('question')
+    question = request.args.get('question') if request.args.get('question') is not None else ""
     if len(question) > 0:
         answer = "resposta"
-        # coloca o processamento da rede aqui
+        # coloca o processamento da rede aq
         context = answer
     return render_template('index.html', context=context)
 
