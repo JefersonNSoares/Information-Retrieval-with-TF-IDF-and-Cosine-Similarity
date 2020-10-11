f0cac471fc6ab94d326a0def7d54b8b516f691c5
gustavo <gustavorw03@gmail.com>
2020-10-10 21:28:47 -0300

    finalizado v1

--- QA/app.py
+++ QA/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__ , static_url_path='/static')
-@app.route("/", methods=['GET'])
+app = Flask(__name__ )
+@app.route("/", methods=['GET', 'POST'])
 def index():
     context = ''
-    question = request.args.get('question') if request.args.get('question') is not None else ""
-    if len(question) > 0:
-        answer = "resposta"
-        # coloca o processamento da rede aq
-        context = answer
+    if request.method == 'POST':
+        question = request.form.get('question') if request.form.get('question') is not None else ""
+        print(question)
+        if len(question) > 0:
+            answer = question
+            # coloca o processamento da rede aq
+            context = answer
     return render_template('index.html', context=context)
 
 
